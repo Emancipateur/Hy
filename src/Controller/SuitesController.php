1cb037d5beee9669b02bfb240877443312a99d6f
Kevin Cenerino <cenerinok@gmail.com>
2022-03-14 17:31:20 +0100

    7etabli + nav

--- src/Controller/SuitesController.php
+++ src/Controller/SuitesController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use App\Entity\Images;
 use App\Entity\Suites;
 use App\Form\SuitesType;
-use App\Repository\SuitesRepository;
 use Doctrine\ORM\EntityManager;
+use App\Repository\SuitesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/suites')]
@@ -58,6 +59,25 @@ class SuitesController extends AbstractController
         ]);
     }
 
+#[Route('/supprime/image/{id}', name: 'annonces_delete_image', methods: ['POST','GET'])]
+public function deleteImage(Images $image, Request $request, EntityManagerInterface $entityManager){
+
+
+  
+        $nom = $image->getTitre();
+        // On supprime le fichier
+        unlink($this->getParameter('images_directory').'/'.$nom);
+
+        // On supprime l'entrée de la base
+        $entityManager->remove($image);
+        $entityManager->flush();
+
+        return $this->redirect($request->headers->get('referer'));
+      
+        
+    
+}
+
     #[Route('/{id}', name: 'app_suites_show', methods: ['GET'])]
     public function show(Suites $suite): Response
     {
@@ -67,12 +87,28 @@ class SuitesController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_suites_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Suites $suite, SuitesRepository $suitesRepository): Response
+    public function edit(Request $request, Suites $suite, SuitesRepository $suitesRepository, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(SuitesType::class, $suite);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $images = $form->get('images')->getData();
+            foreach($images as $image){
+                $fichier = md5(uniqid()).'.'.$image->guessExtension();
+                $image->move(
+                    $this->getParameter('images_directory'),
+                    $fichier
+                );
+                
+                // On crée l'image dans la base de données
+                $img = new Images();
+                $img->setTitre($fichier);
+                $suite->addImage($img);
+            }
+            
+            $entityManager->persist($suite);
+            $entityManager->flush();
             $suitesRepository->add($suite);
             return $this->redirectToRoute('app_suites_index', [], Response::HTTP_SEE_OTHER);
         }
